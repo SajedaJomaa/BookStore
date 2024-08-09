fe3c6d5c7d2bb9aa28fda7d6b77f09cd52fe2776
SajedaJomaa <97788762+SajedaJomaa@users.noreply.github.com>
2024-08-09 12:42:25 +0300

    Update Purchased.js

--- frontend/src/components/PurchasedBooks/Purchased.js
+++ frontend/src/components/PurchasedBooks/Purchased.js
@@ -2,6 +2,7 @@
 import React, { useContext } from 'react';
 import BookContext from '../../contexts/BookContext.js';
 import classes from './Purchased.module.css';
+import defaultImage from "../../assets/139110060-isometric-design-of-books-and-coffee-mug-over-white-background-vector-illustration-removebg-preview.png";
 
 export default function Purchased() {
     const { purchasedBooks } = useContext(BookContext);
@@ -19,7 +20,11 @@ export default function Purchased() {
                             return (
                                 <li key={index}>
                                     <button className={classes.purchdata}>
-                                        <img src={book.volumeInfo.imageLinks?.thumbnail} alt={book.volumeInfo.title} className={classes.bookimage} />
+                                        <img
+                                            src={book.volumeInfo?.imageLinks?.thumbnail || { defaultImage }}
+                                            alt={book.volumeInfo?.title || 'No Title'}
+                                            className={classes.bookimage}
+                                        />
                                         <p>{book.volumeInfo.title}</p>
                                     </button>
                                 </li>
